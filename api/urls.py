1d06cd7c05ca2a950eec61c6ca80cef91e6ac2be
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-12-09 10:26:02 +0500

    topic LL

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import Quiz_all,Topic_all
+from .views import Quiz_all,Topic_all,Question_all
 
 urlpatterns = [
     path('quiz/', Quiz_all.as_view()),
     path('topic/<int:pk>/', Topic_all.as_view()),
+    path('question/<int:pk>/', Question_all.as_view()),
 ]
